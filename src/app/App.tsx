b2c951955ae456dcb6d116c78dbaf1837365e7e4
jun <jun@junho.co>
2026-07-11 13:00:40 -0400

    Add PDF export for meeting notes

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -31,6 +31,7 @@ import { MoveNoteToFolderDialog } from "../components/folders/MoveNoteToFolderDi
 import { MoveSessionToProjectDialog } from "../components/folders/MoveSessionToProjectDialog";
 import { NoteEditor } from "../components/note-editor/NoteEditor";
 import { NoteHeaderActions } from "../components/note-editor/NoteHeaderActions";
+import { exportNoteAsPdf } from "../lib/note-pdf";
 import { NoteChatPanel } from "../components/note-chat/NoteChatPanel";
 import { useNoteChat } from "../components/note-chat/useNoteChat";
 import { GlobalRecorderPill } from "../components/recorder/GlobalRecorderPill";
@@ -717,6 +718,7 @@ export function App() {
       askJuneOpen={noteChatOpen}
       askJuneWorking={noteChat.working}
       onAskJune={() => setNoteChatOpen((open) => !open)}
+      onExportPdf={() => exportNoteAsPdf(selectedNote.title)}
       onDelete={() => setConfirmDeleteNote(true)}
     />
   ) : null;
